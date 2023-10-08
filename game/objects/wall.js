fe5120f3455c1ba40d80233b365687533e0ee272
alesan99 <alesanesp99@gmail.com>
2023-10-07 21:33:53 -0500

    Init walls

---
+++ game/objects/wall.js
@@ -0,0 +1,14 @@
+//Walls, collides with other objects to stop them
+
+class Walls {
+	//Initialize: x pos, y pos, width, height
+	constructor (config) {
+        this.config = config // configuration string
+        this.polys = [] //list of polygons
+	}
+
+    //Check if object is inside walls
+    checkInside(obj) {
+        
+    }
+}
