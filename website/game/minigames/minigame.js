af52afb01692ec6941094a7f391e07292a4334e7
alesan99 <alesanesp99@gmail.com>
2023-11-06 23:12:32 -0600

    Init Minigame Added dialogue line

---
+++ website/game/minigames/minigame.js
@@ -0,0 +1,33 @@
+class Minigame {
+    constructor() {
+
+    }
+
+    load() {
+
+    }
+
+    update(dt) {
+
+    }
+
+    draw() {
+
+    }
+
+    exit() {
+
+    }
+
+    keyPress(key) {
+
+    }
+
+    keyRelease(key) {
+        
+    }
+
+    mouseClick(x, y, button) {
+
+    }
+}
