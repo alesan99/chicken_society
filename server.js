3c43050b502021188e662eb9a8192e6613ad4fd5
Mikey C <mc123dj@ku.edu>
2023-11-17 11:42:36 -0600

    DB_Work

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const app = express();
 const http = require("http"); // Used to start server
 const server = http.createServer(app);
 
-const { Server } = require("socket.io");
 const msgpack = require("socket.io-msgpack-parser"); // Import the socket.io-msgpack-parser module. If this crashes run "npm ci" again
 const io = new Server(server, {
 	parser: msgpack // Use msgpack for faster serialization
@@ -47,6 +46,35 @@ const {} = require("./server/requests.js");
 // Start server on port
 const localIPAddress = "localhost" // ipv4 //"10.104.58.91" // IPv4 or localhost
 const port = 3000
+db.initializeDB;
+db.query("SHOW TABLES like 'user'", (err, result, fields) => {
+	// if (err) throw err;
+	// console.log(result);
+	if (result.length == 0) {
+		console.log("No user found");
+		const createTableQuery = `
+        CREATE TABLE user (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          username VARCHAR(255) NOT NULL,
+          color VARCHAR(255) NOT NULL,
+		  red TINYINT UNSIGNED NOT NULL,
+		  blue TINYINT UNSIGNED NOT NULL,
+		  green TINYINT UNSIGNED NOT NULL,
+		  hat VARCHAR(255) NOT NULL,
+		  accessory VARCHAR(255) NOT NULL,
+		  money INT UNSIGNED NOT NULL
+        )`;
+		db.query(createTableQuery, (createError) => {
+			if (createError) {
+			}
+			console.log('User table created');
+			});
+			}
+			else {
+		  		console.log('User table already exists');
+			}
+	});
+db.query("SELECT * FROM user");
 server.listen(port, localIPAddress, () => {
 	console.log(`Server is running on http://${localIPAddress}:${port}`)
 })
