b5afd920938b713edf4752cf07aa596fe460895e
cabbidge <121267327+cabbidge@users.noreply.github.com>
2025-04-27 19:03:09 -0500

    Old Town music by Thad

--- website/game/assets.js
+++ website/game/assets.js
@@ -71,6 +71,7 @@ function loadGameAssets() {
 	MUSIC.crossroadstrack = AudioSystem.newMusic("assets/music/crossroadstrack.mp3");
 	MUSIC.pldistrictsong = AudioSystem.newMusic("assets/music/pldistrictsong.mp3");
 	MUSIC.thefunnychicken = AudioSystem.newMusic("assets/music/thefunnychicken.mp3");
+	MUSIC.oldtownsong = AudioSystem.newMusic("assets/music/oldtownsong.mp3");
 
 	SFX.door = AudioSystem.newSound("assets/sfx/door.ogg");
 	SFX.woosh = AudioSystem.newSound("assets/sfx/woosh.ogg");
